773514600ed7fa83af12c366fffc937445f476d2
Sergey Elpashev <elpashevs@bk.ru>
2023-12-02 21:18:01 +0300

    Initial

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    // bg-[#303952]
+    <div className="bg-gradient-to-tr from-[#e15f41] to-[#786fa6] w-screen h-screen">
+      <div className="bg-[#596275] absolute top-[50%] left-[50%] md:h-[500px] md:w-[1000px] transform -translate-x-1/2 -translate-y-1/2 rounded-[50px] w-[300px] h-[60%]"></div>
+    </div>
+  );
+}
+
+export default App;
